cbabf3068e29a52e11985811937db9ff6a8d0a2d
spirilis <spirilis@spirilis.net>
2015-12-30 13:14:20 -0500

    Moving along with doxygen commenting...

--- SLFS.h
+++ SLFS.h
@@ -54,15 +54,16 @@
 #include <WiFi.h>
 
 // Errno values intrinsic to this library (not the SimpleLink API)
-/** @defgroup slfs_errnos SLFS Library-native Error Codes
- * @{
- */
-#define SLFS_LIB_ERR_FILE_NOT_OPEN         -10001  /**< File operation performed when SerFlash object has no file open */
-#define SLFS_LIB_ERR_FILE_OPEN_FOR_WRITE   -10002  /**< File operation requires read-mode but the SerFlash object has a file open for write */
-#define SLFS_LIB_ERR_FILE_OPEN_FOR_READ    -10003  /**< File operation requires write-mode but the SerFlash object has a file open for read */
-#define SLFS_LIB_ERR_FILE_ALREADY_OPEN     -10004  /**< File operation performed when SerFlash object has a file open */
-#define SLFS_LIB_ERR_OFFSET_OUT_OF_BOUNDS  -10005  /**< File position seek attempted to out-of-bounds position */
-/** @} */
+/// @defgroup slfs_errnos SLFS Library-native Error Codes
+/// @{
+
+#define SLFS_LIB_ERR_FILE_NOT_OPEN         -10001  ///< File operation intended for an open file when SerFlash object has no file open
+#define SLFS_LIB_ERR_FILE_OPEN_FOR_WRITE   -10002  ///< File operation requires read-mode but the SerFlash object has a file open for write
+#define SLFS_LIB_ERR_FILE_OPEN_FOR_READ    -10003  ///< File operation requires write-mode but the SerFlash object has a file open for read
+#define SLFS_LIB_ERR_FILE_ALREADY_OPEN     -10004  ///< File operation requiring an unreferenced or unopen file when SerFlash object has a file open */
+#define SLFS_LIB_ERR_OFFSET_OUT_OF_BOUNDS  -10005  ///< File position seek attempted to out-of-bounds position
+
+/// @}
 
 
 class SLFS : public Stream {
@@ -74,18 +75,103 @@ class SLFS : public Stream {
 
         
     public:
-        SLFS();
+        SLFS();  ///< Constructor
+        /// @defgroup admin_funcs Administrative and whole-file operation functions
+        /// @{
+
+        ///
+        /// @brief Initialize
+        /// @details Ensure the SimpleLink API is initialized using WiFi.init()
         void begin(void);
+
+        ///
+        /// @brief Stop
+        /// @details Does nothing, just included per Arduino API convention
         void end(void);
+
+        ///
+        /// @brief Open file
+        /// @details Open a file for create, read or write as specified by @c mode
+        /// @param filename String representing a file. @n
+        ///        While the SimpleLink Filesystem has no intrinsic concept of directories,
+        ///        you may use slashes in your filename to organize files in a conceptual
+        ///        directory structure (and TI's system files utilize a UNIX-style forward-slash
+        ///        convention for their own files likewise).
+        /// @param mode SimpleLink API-native mode specifier.
+        ///        @parblock
+        ///        The following parameters are allowed for opening files:
+        ///        @par SimpleLink Filesystem API modes
+        ///        @c FS_MODE_OPEN_READ - Open file for reading  @n
+        ///        @c FS_MODE_OPEN_WRITE - Open existing file for writing @n
+        ///        @c FS_MODE_OPEN_CREATE(size,opts) - Create a new file, destroying the old one if an existing file with identical filename existed. @n
+        ///        @par FS_MODE_OPEN_CREATE parameters
+        ///        @c size - Maximum size of file, in bytes.  Note the SimpleLink Filesystem allocates using a minimal block size of 4KB. @n
+        ///        @c opts - A bitwise OR of the following attributes: @n
+        ///        @c _FS_FILE_OPEN_FLAG_COMMIT - File is mirrored on Serial Flash; this causes the file to use 2X the amount of space. @n
+        ///        @c _FS_FILE_OPEN_FLAG_SECURE - File is stored securely.  No documentation on what this means. @n
+        ///        @c _FS_FILE_OPEN_FLAG_NO_SIGNATURE_TEST - Something related to Secure storage.  No documentation on what this means. @n
+        ///        @c _FS_FILE_OPEN_FLAG_STATIC - Something related to Secure storage.  No documentation on what this means. @n
+        ///        @c _FS_FILE_OPEN_FLAG_VENDOR - Something related to Secure storage.  No documentation on what this means. @n
+        ///        @c _FS_FILE_PUBLIC_WRITE - Something related to Secure storage.  No documentation on what this means. @n
+        ///        @c _FS_FILE_PUBLIC_READ - Something related to Secure storage.  No documentation on what this means. @n
+        ///        @endparblock
+        ///
         int32_t open(const uint8_t *filename, int32_t mode);
-        inline int32_t open(const char *filename, int32_t mode);
+
+        ///
+        /// @brief Close file
+        /// @details Close a previously-opened file
+        /// @returns SL_FS_OK if successful, negative number if error
         int32_t close(void);
+
+        ///
+        /// @brief Delete file
+        /// @details Delete a file by filename.
+        /// @n
+        /// While it is strictly possible per the SimpleLink Filesystem API to delete a file that is
+        /// different from the one opened, this library has no way to determine whether the file
+        /// specified for deletion is the same as the file presently open (we don't track the filename),
+        /// so we default to disallowing any use of the del() function while a file is presently open.
+        /// @n @n
+        /// If necessary, you may declare another instance of the SLFS class in your code and use that
+        /// object to perform a del() against a different file.
         int32_t del(const uint8_t *filename);
         inline int32_t del(const char *filename);
+        /// @}
+        ///
 
+        ///
+        /// @defgroup errors Error reporting
+        /// @{
+
+        ///
+        /// @brief Return errno code of last API call
+        /// @returns Error code, typically <= 0 in value.
         int32_t lastError(void);
-        const char *getErrorString(int32_t);
+        ///
+        /// @brief Error string description
+        /// @details Get meaningful string description of the errno code supplied
+        /// @param errno Error code, typically <= 0 in value.
+        /// @returns const char string representing meaningful description of given errno code, or "ERR_UNKNOWN" if it's not a known error code.
+        const char *getErrorString(int32_t errno);
+
+        ///
+        /// @brief Error string description of last API call
+        /// @details Variant of getErrorString() which uses the class's built-in @c retval parameter which represents the return code
+        ///          for the last executed SimpleLink API call or last SLFS API call.
+        /// @note The value of @c retval usually matches the last SimpleLink API call, but may be assigned instead to an SLFS-native
+        ///       errno code if the last error was caused by the SLFS's own bounds-checking and sanity-checking code. @n
+        ///       For example, if the SLFS object had no open file and the user performed an open() on a new file, the @c retval
+        ///       variable would contain the return value of the sl_FsOpen() SimpleLink API syscall performed to open the file. @n
+        ///       But if the user was running del() on a file when the SLFS object had a file already open, the @c retval parameter would
+        ///       not contain any return code derived from a SimpleLink API syscall such as sl_FsDel(), instead it would contain the value
+        ///       SLFS_LIB_ERR_FILE_ALREADY_OPEN which was produced by the sanity-checking code within SLFS's del() function.
+        ///       Under this circumstance the last-known error code derived from a SimpleLink API syscall (as opposed to any SLFS-based
+        ///       function) would no longer be known.
+        /// @returns const char string representing meaningful description of last API return code, or "ERR_UNKNOWN" if it's not a known error code.
         const char *lastErrorString(void);
+        /// @}
+        ///
 
         virtual int32_t seek(int32_t);
         virtual size_t size(void);
